7157f2af3518d59fce29155db20da5719b35e812
parkchanwong <cksdnd5433@gmail.com>
2026-09-16 14:55:57 +0900

    feat: 국가 등록 service code

--- src/main/java/com/ohgiraffers/quote_collection_spring/service/category/CountryService.java
+++ src/main/java/com/ohgiraffers/quote_collection_spring/service/category/CountryService.java
@@ -2,9 +2,13 @@ package com.ohgiraffers.quote_collection_spring.service.category;
 
 import com.ohgiraffers.quote_collection_spring.dto.category.CountryDTO;
 import com.ohgiraffers.quote_collection_spring.entity.category.CountryEntity;
-import com.ohgiraffers.quote_collection_spring.exception.NotFoundCountryException;
+import com.ohgiraffers.quote_collection_spring.exception.category.country.DuplicateCountryException;
+import com.ohgiraffers.quote_collection_spring.exception.category.country.EmptyCountryException;
+import com.ohgiraffers.quote_collection_spring.exception.category.country.NotFoundCountryException;
 import com.ohgiraffers.quote_collection_spring.repository.category.CountryRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,14 +22,21 @@ public class CountryService {
         this.countryRepository = countryRepository;
     }
 
-    public CountryDTO ConvertToDTO(CountryEntity countryEntity){
+    public CountryDTO convertToDTO(CountryEntity countryEntity){
         return new CountryDTO(countryEntity.getId(), countryEntity.getName());
+    }
+
+    public CountryEntity convertToEntity(CountryDTO countryDTO){
+        CountryEntity countryEntity = new CountryEntity();
+        countryEntity.setName(countryDTO.getName());
+
+        return countryEntity;
     }
 
     public List<CountryDTO> findAllCountries() {
         List<CountryEntity> countries = countryRepository.findAll();
 
-        return countries.stream().map(this::ConvertToDTO).toList();
+        return countries.stream().map(this::convertToDTO).toList();
     }
 
     public CountryDTO findCountryById(int countryId){
@@ -33,6 +44,17 @@ public class CountryService {
                 .findById(countryId)
                 .orElseThrow(NotFoundCountryException::new);
 
-        return ConvertToDTO(countryEntity);
+        return convertToDTO(countryEntity);
+    }
+
+    @Transactional
+    public void saveCountry(CountryDTO countryDTO){
+        if (countryDTO.getName() == null || countryDTO.getName().isBlank()){
+            throw new EmptyCountryException();
+        } else if (countryRepository.existsByName(countryDTO.getName())){
+            throw new DuplicateCountryException();
+        }
+
+        countryRepository.save(convertToEntity(countryDTO));
     }
 }
