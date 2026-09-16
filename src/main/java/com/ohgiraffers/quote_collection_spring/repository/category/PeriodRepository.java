acba8dd85f8582b392ebd2886638121195d6b54a
parkchanwong <cksdnd5433@gmail.com>
2026-09-16 16:55:15 +0900

    feat: add 시대 중복 repository 코드

--- src/main/java/com/ohgiraffers/quote_collection_spring/repository/category/PeriodRepository.java
+++ src/main/java/com/ohgiraffers/quote_collection_spring/repository/category/PeriodRepository.java
@@ -4,4 +4,5 @@ import com.ohgiraffers.quote_collection_spring.entity.category.PeriodEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PeriodRepository extends JpaRepository<PeriodEntity, Integer> {
+    boolean existsByName(String code);
 }
