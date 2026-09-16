4d0e39ff83889734bea773fc8f8e55f9e14e0046
parkchanwong <cksdnd5433@gmail.com>
2026-09-16 17:35:44 +0900

    feat: add 분야 전체 조회 controller 코드

---
+++ src/main/java/com/ohgiraffers/quote_collection_spring/controller/category/FieldController.java
@@ -0,0 +1,35 @@
+package com.ohgiraffers.quote_collection_spring.controller.category;
+
+import com.ohgiraffers.quote_collection_spring.common.ResponseList;
+import com.ohgiraffers.quote_collection_spring.dto.category.FieldDTO;
+import com.ohgiraffers.quote_collection_spring.service.category.FieldService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/field")
+public class FieldController {
+    private final FieldService fieldService;
+
+    public FieldController(FieldService fieldService) {
+        this.fieldService = fieldService;
+    }
+
+    // 전체 분야 조회
+    @GetMapping
+    public ResponseEntity<ResponseList> findAllFields() {
+        List<FieldDTO> fields = fieldService.findAllFields();
+
+        ResponseList responseList = new ResponseList(
+                HttpStatus.OK.value(),
+                fields
+        );
+
+        return new ResponseEntity<>(responseList, HttpStatus.OK);
+    }
+}
