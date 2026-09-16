9abb5cceec17bf571bfe19b2c217f57ba3ec9c72
parkchanwong <cksdnd5433@gmail.com>
2026-09-16 14:53:54 +0900

    refactor: 맨 마지막 잡지 못한 예외 및 에러 잡는 코드만 남겨둠

--- src/main/java/com/ohgiraffers/quote_collection_spring/exception/ExceptionController.java
+++ src/main/java/com/ohgiraffers/quote_collection_spring/exception/ExceptionController.java
@@ -1,6 +1,7 @@
 package com.ohgiraffers.quote_collection_spring.exception;
 
 import com.ohgiraffers.quote_collection_spring.common.ResponseError;
+import com.ohgiraffers.quote_collection_spring.exception.category.country.NotFoundCountryException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,16 +9,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ExceptionController {
-    @ExceptionHandler(NotFoundCountryException.class)
-    public ResponseEntity<ResponseError> notFoundCountry(NotFoundCountryException e) {
-        ResponseError responseError = new ResponseError(
-                HttpStatus.NOT_FOUND.value(),
-                e.getMessage()
-        );
-
-        return new ResponseEntity<>(responseError, HttpStatus.NOT_FOUND);
-    }
-
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ResponseError> handleException(Exception e){
         ResponseError responseError = new ResponseError(
