c834f9b52c98a99ccca6f67676fd782440dbf44f
parkchanwong <cksdnd5433@gmail.com>
2026-09-16 17:24:17 +0900

    feat: add field repository

---
+++ src/main/java/com/ohgiraffers/quote_collection_spring/repository/category/FieldRepository.java
@@ -0,0 +1,7 @@
+package com.ohgiraffers.quote_collection_spring.repository.category;
+
+import com.ohgiraffers.quote_collection_spring.entity.category.FieldEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FieldRepository extends JpaRepository<FieldEntity, Integer> {
+}
