752b0518ce27d8940503b1e46543f71dfc5ee4ce
parkchanwong <cksdnd5433@gmail.com>
2026-09-16 17:26:47 +0900

    feat: add repository annotation

--- src/main/java/com/ohgiraffers/quote_collection_spring/repository/category/FieldRepository.java
+++ src/main/java/com/ohgiraffers/quote_collection_spring/repository/category/FieldRepository.java
@@ -2,6 +2,8 @@ package com.ohgiraffers.quote_collection_spring.repository.category;
 
 import com.ohgiraffers.quote_collection_spring.entity.category.FieldEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FieldRepository extends JpaRepository<FieldEntity, Integer> {
 }
--- src/main/java/com/ohgiraffers/quote_collection_spring/repository/category/PeriodRepository.java
+++ src/main/java/com/ohgiraffers/quote_collection_spring/repository/category/PeriodRepository.java
@@ -2,7 +2,9 @@ package com.ohgiraffers.quote_collection_spring.repository.category;
 
 import com.ohgiraffers.quote_collection_spring.entity.category.PeriodEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PeriodRepository extends JpaRepository<PeriodEntity, Integer> {
     boolean existsByName(String name);
 }
