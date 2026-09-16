c0ba1deaa0c75b370a0802d0ff3bfcca79b01631
parkchanwong <cksdnd5433@gmail.com>
2026-09-16 18:00:26 +0900

    feat: add 분야 등록 빈값 예외, 중복 예외 controller에 추가

--- src/main/java/com/ohgiraffers/quote_collection_spring/exception/ExceptionController.java
+++ src/main/java/com/ohgiraffers/quote_collection_spring/exception/ExceptionController.java
@@ -1,6 +1,8 @@
 package com.ohgiraffers.quote_collection_spring.exception;
 
 import com.ohgiraffers.quote_collection_spring.common.ResponseError;
+import com.ohgiraffers.quote_collection_spring.exception.category.Field.DuplicateFieldException;
+import com.ohgiraffers.quote_collection_spring.exception.category.Field.EmptyFieldException;
 import com.ohgiraffers.quote_collection_spring.exception.category.Field.NotFoundFieldException;
 import com.ohgiraffers.quote_collection_spring.exception.category.country.DuplicateCountryException;
 import com.ohgiraffers.quote_collection_spring.exception.category.country.EmptyCountryException;
@@ -33,9 +35,10 @@ public class ExceptionController {
     // 필수값을 빈값으로 등록
     @ExceptionHandler({
             EmptyCountryException.class,
-            EmptyPeriodException.class
+            EmptyPeriodException.class,
+            EmptyFieldException.class
     })
-    public ResponseEntity<ResponseError> emptyCountry(Exception e) {
+    public ResponseEntity<ResponseError> emptyException(Exception e) {
         ResponseError responseError = new ResponseError(
                 HttpStatus.BAD_REQUEST.value(),
                 e.getMessage()
@@ -47,9 +50,10 @@ public class ExceptionController {
     // 중복 등록
     @ExceptionHandler({
             DuplicateCountryException.class,
-            DuplicatePeriodException.class
+            DuplicatePeriodException.class,
+            DuplicateFieldException.class
     })
-    public ResponseEntity<ResponseError> duplicateCountry(DuplicateCountryException e) {
+    public ResponseEntity<ResponseError> duplicateException(Exception e) {
         ResponseError responseError = new ResponseError(
                 HttpStatus.CONFLICT.value(),
                 e.getMessage()
