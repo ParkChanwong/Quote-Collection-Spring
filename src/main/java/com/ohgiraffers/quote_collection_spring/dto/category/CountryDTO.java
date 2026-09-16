6acb663f841072d80a6c5c543272792e30ba6953
parkchanwong <cksdnd5433@gmail.com>
2026-09-16 10:56:03 +0900

    test commit

---
+++ src/main/java/com/ohgiraffers/quote_collection_spring/dto/category/CountryDTO.java
@@ -0,0 +1,38 @@
+package com.ohgiraffers.quote_collection_spring.dto.category;
+
+public class CountryDTO {
+    private int id;
+    private String name;
+
+    public CountryDTO() {
+    }
+
+    public CountryDTO(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "CountryDTO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/com/ohgiraffers/quote_collection_spring/QuoteCollectionSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.quote_collection_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteCollectionSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
