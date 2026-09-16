d91cefabc8fbe55e1060a64f6eb6804fa67e9b30
parkchanwong <cksdnd5433@gmail.com>
2026-09-16 15:56:42 +0900

    feat: add 시대 전체 조회 Service 코드

---
+++ src/main/java/com/ohgiraffers/quote_collection_spring/service/category/PeriodService.java
@@ -0,0 +1,30 @@
+package com.ohgiraffers.quote_collection_spring.service.category;
+
+import com.ohgiraffers.quote_collection_spring.dto.category.PeriodDTO;
+import com.ohgiraffers.quote_collection_spring.entity.category.PeriodEntity;
+import com.ohgiraffers.quote_collection_spring.repository.category.PeriodRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PeriodService {
+    private PeriodRepository periodRepository;
+
+    @Autowired
+    public PeriodService(PeriodRepository periodRepository) {
+        this.periodRepository = periodRepository;
+    }
+
+    public PeriodDTO convertToDTO(PeriodEntity periodEntity) {
+        return new PeriodDTO(periodEntity.getId(), periodEntity.getName());
+    }
+
+    public List<PeriodDTO> findAllPeriods() {
+        List<PeriodEntity> periods = periodRepository.findAll(Sort.by(Sort.Direction.ASC, "name"));
+
+        return periods.stream().map(this::convertToDTO).toList();
+    }
+}
