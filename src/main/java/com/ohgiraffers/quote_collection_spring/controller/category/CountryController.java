9b54dae5eff5e4c56abccc1fc64d9d3bc7ac4579
parkchanwong <cksdnd5433@gmail.com>
2026-09-16 13:37:36 +0900

    feat: add country get find by id controller

--- src/main/java/com/ohgiraffers/quote_collection_spring/controller/category/CountryController.java
+++ src/main/java/com/ohgiraffers/quote_collection_spring/controller/category/CountryController.java
@@ -1,11 +1,13 @@
 package com.ohgiraffers.quote_collection_spring.controller.category;
 
 import com.ohgiraffers.quote_collection_spring.common.ResponseList;
+import com.ohgiraffers.quote_collection_spring.common.ResponseSingle;
 import com.ohgiraffers.quote_collection_spring.dto.category.CountryDTO;
 import com.ohgiraffers.quote_collection_spring.service.category.CountryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +33,17 @@ public class CountryController {
         );
 
         return ResponseEntity.status(HttpStatus.OK).body(responseList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseSingle> findCountryById(@PathVariable int id){
+        CountryDTO countryDTO = countryService.findCountryById(id);
+
+        ResponseSingle responseSingle = new ResponseSingle(
+                HttpStatus.OK.value(),
+                countryDTO
+        );
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseSingle);
     }
 }
