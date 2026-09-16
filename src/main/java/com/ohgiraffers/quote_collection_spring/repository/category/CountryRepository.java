735ddc72baaf24ab16cb5adba981db0101a0313c
parkchanwong <cksdnd5433@gmail.com>
2026-09-16 14:55:36 +0900

    feat: 국가 이름 중복 검사 repository 추가

--- src/main/java/com/ohgiraffers/quote_collection_spring/repository/category/CountryRepository.java
+++ src/main/java/com/ohgiraffers/quote_collection_spring/repository/category/CountryRepository.java
@@ -4,4 +4,5 @@ import com.ohgiraffers.quote_collection_spring.entity.category.CountryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CountryRepository extends JpaRepository<CountryEntity, Integer> {
+    boolean existsByName(String name);
 }
