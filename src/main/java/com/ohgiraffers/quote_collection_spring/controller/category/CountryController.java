ee6ec5562f70e5bf225c8eba25518ec0aff6f80a
parkchanwong <cksdnd5433@gmail.com>
2026-09-16 12:06:01 +0900

    feat: add find all countries

---
+++ src/main/java/com/ohgiraffers/quote_collection_spring/controller/category/CountryController.java
@@ -0,0 +1,35 @@
+package com.ohgiraffers.quote_collection_spring.controller.category;
+
+import com.ohgiraffers.quote_collection_spring.common.ResponseList;
+import com.ohgiraffers.quote_collection_spring.dto.category.CountryDTO;
+import com.ohgiraffers.quote_collection_spring.service.category.CountryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/country")
+public class CountryController {
+    private final CountryService countryService;
+
+    public CountryController(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
+    // 전체 국가 조회
+    @GetMapping
+    public ResponseEntity<ResponseList> findAllCountries() {
+        List<CountryDTO> contries = countryService.findAllCountries();
+
+        ResponseList responseList = new ResponseList(
+                HttpStatus.OK.value(),
+                contries
+        );
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseList);
+    }
+}
