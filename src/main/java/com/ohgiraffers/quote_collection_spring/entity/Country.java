8e85d6c2d1bc7d27f4db0d30bc564ab2c3940a08
parkchanwong <cksdnd5433@gmail.com>
2026-09-16 11:05:07 +0900

    feat: add country entity

---
+++ src/main/java/com/ohgiraffers/quote_collection_spring/entity/Country.java
@@ -0,0 +1,45 @@
+package com.ohgiraffers.quote_collection_spring.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "country")
+public class Country {
+    @Id
+    @Column(name = "country_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "country_name")
+    private String name;
+
+    public Country() {}
+
+    public Country(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
