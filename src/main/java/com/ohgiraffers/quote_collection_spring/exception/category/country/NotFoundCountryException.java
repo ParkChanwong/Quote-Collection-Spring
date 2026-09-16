e12934aa787096a3c1e2a4efc81752f44fc98291
parkchanwong <cksdnd5433@gmail.com>
2026-09-16 14:53:04 +0900

    refactor: exception.category.country 패키지로 이동

--- src/main/java/com/ohgiraffers/quote_collection_spring/exception/category/country/NotFoundCountryException.java
+++ src/main/java/com/ohgiraffers/quote_collection_spring/exception/category/country/NotFoundCountryException.java
@@ -1,4 +1,4 @@
-package com.ohgiraffers.quote_collection_spring.exception;
+package com.ohgiraffers.quote_collection_spring.exception.category.country;
 
 public class NotFoundCountryException extends RuntimeException {
     public NotFoundCountryException(String message) {
