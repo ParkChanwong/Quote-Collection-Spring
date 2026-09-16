66c34006ec7ff816361e30e80ec20bcd044ec8ab
parkchanwong <cksdnd5433@gmail.com>
2026-09-16 11:45:07 +0900

    feat: add country find all method and convert to dto method, country service file

---
+++ src/main/java/com/ohgiraffers/quote_collection_spring/service/category/CountryService.java
@@ -0,0 +1,29 @@
+package com.ohgiraffers.quote_collection_spring.service.category;
+
+import com.ohgiraffers.quote_collection_spring.dto.category.CountryDTO;
+import com.ohgiraffers.quote_collection_spring.entity.category.CountryEntity;
+import com.ohgiraffers.quote_collection_spring.repository.category.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CountryService {
+    private final CountryRepository countryRepository;
+
+    @Autowired
+    public CountryService(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    public CountryDTO ConvertToDTO(CountryEntity countryEntity){
+        return new CountryDTO(countryEntity.getId(), countryEntity.getName());
+    }
+
+    public List<CountryDTO> findAllCountries() {
+        List<CountryEntity> countries = countryRepository.findAll();
+
+        return countries.stream().map(this::ConvertToDTO).toList();
+    }
+}
