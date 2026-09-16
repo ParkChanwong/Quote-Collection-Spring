f71a3ea513baa4ae070dadb7e3a050e7d6c2de67
parkchanwong <cksdnd5433@gmail.com>
2026-09-16 17:21:26 +0900

    feat: add 분야 DTO

---
+++ src/main/java/com/ohgiraffers/quote_collection_spring/dto/category/FieldDTO.java
@@ -0,0 +1,37 @@
+package com.ohgiraffers.quote_collection_spring.dto.category;
+
+public class FieldDTO {
+    private int id;
+    private String name;
+
+    public FieldDTO() {}
+
+    public FieldDTO(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "FieldDTO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
