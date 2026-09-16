670e01c4c68dfa7c6b557616763205795093fedf
parkchanwong <cksdnd5433@gmail.com>
2026-09-16 17:59:58 +0900

    feat: add 분야 등록 중복 repository

--- src/main/java/com/ohgiraffers/quote_collection_spring/repository/category/FieldRepository.java
+++ src/main/java/com/ohgiraffers/quote_collection_spring/repository/category/FieldRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FieldRepository extends JpaRepository<FieldEntity, Integer> {
+    boolean existsByName(String name);
 }
