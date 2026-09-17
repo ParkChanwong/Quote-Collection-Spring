c21f4da7f6ba59e51ec26ac469d985c9b1a09cb8
parkchanwong <cksdnd5433@gmail.com>
2026-09-17 10:52:30 +0900

    feat: add person Controller

---
+++ src/main/java/com/ohgiraffers/quote_collection_spring/controller/person/PersonController.java
@@ -0,0 +1,19 @@
+package com.ohgiraffers.quote_collection_spring.controller.person;
+
+import com.ohgiraffers.quote_collection_spring.service.person.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+    private final PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+
+}
