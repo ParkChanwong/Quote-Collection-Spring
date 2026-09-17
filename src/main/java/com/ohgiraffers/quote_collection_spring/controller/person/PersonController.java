3ab87dbe07f446252c387efe0ba338dd88e34947
parkchanwong <cksdnd5433@gmail.com>
2026-09-17 11:18:33 +0900

    feat: add person find all controller code

--- src/main/java/com/ohgiraffers/quote_collection_spring/controller/person/PersonController.java
+++ src/main/java/com/ohgiraffers/quote_collection_spring/controller/person/PersonController.java
@@ -1,9 +1,16 @@
 package com.ohgiraffers.quote_collection_spring.controller.person;
 
+import com.ohgiraffers.quote_collection_spring.common.ResponseList;
+import com.ohgiraffers.quote_collection_spring.dto.person.PersonResponseDTO;
 import com.ohgiraffers.quote_collection_spring.service.person.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/person")
@@ -15,5 +22,15 @@ public class PersonController {
         this.personService = personService;
     }
 
+    @GetMapping
+    public ResponseEntity<ResponseList> findAllPersons() {
+        List<PersonResponseDTO> persons = personService.findAllPersons();
 
+        ResponseList<PersonResponseDTO> responseList = new ResponseList<>(
+                HttpStatus.OK.value(),
+                persons
+        );
+
+        return ResponseEntity.ok(responseList);
+    }
 }
