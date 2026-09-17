5a3432bcd95737dc135953e7a15e4d914fa49f2e
parkchanwong <cksdnd5433@gmail.com>
2026-09-17 17:09:05 +0900

    feat: add quote find by person name repository

--- src/main/java/com/ohgiraffers/quote_collection_spring/repository/quote/QuoteRepository.java
+++ src/main/java/com/ohgiraffers/quote_collection_spring/repository/quote/QuoteRepository.java
@@ -1,9 +1,14 @@
 package com.ohgiraffers.quote_collection_spring.repository.quote;
 
 import com.ohgiraffers.quote_collection_spring.entity.quote.QuoteEntity;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface QuoteRepository extends JpaRepository<QuoteEntity, Integer> {
+    // 인물명 조회
+    List<QuoteEntity> findAllQuotesByPersonNameContaining(String personName, Sort sort);
 }
