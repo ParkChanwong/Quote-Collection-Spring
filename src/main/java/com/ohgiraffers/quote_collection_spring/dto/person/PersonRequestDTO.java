69e718c69d04229c5c02babbd4f49c735631e76d
parkchanwong <cksdnd5433@gmail.com>
2026-09-17 14:30:50 +0900

    feat: add person Request DTO

---
+++ src/main/java/com/ohgiraffers/quote_collection_spring/dto/person/PersonRequestDTO.java
@@ -0,0 +1,70 @@
+package com.ohgiraffers.quote_collection_spring.dto.person;
+
+public class PersonRequestDTO {
+    private int id;
+    private int countryId;
+    private int periodId;
+    private int fieldId;
+    private String name;
+
+    public PersonRequestDTO() {}
+
+    public PersonRequestDTO(int id, int countryId, int periodId, int fieldId, String name) {
+        this.id = id;
+        this.countryId = countryId;
+        this.periodId = periodId;
+        this.fieldId = fieldId;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(int countryId) {
+        this.countryId = countryId;
+    }
+
+    public int getPeriodId() {
+        return periodId;
+    }
+
+    public void setPeriodId(int periodId) {
+        this.periodId = periodId;
+    }
+
+    public int getFieldId() {
+        return fieldId;
+    }
+
+    public void setFieldId(int fieldId) {
+        this.fieldId = fieldId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonRequestDTO{" +
+                "id=" + id +
+                ", countryId=" + countryId +
+                ", periodId=" + periodId +
+                ", fieldId=" + fieldId +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
