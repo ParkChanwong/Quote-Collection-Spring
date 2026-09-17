77767e9f2226b3e68a888f61d9d5f8a704539934
parkchanwong <cksdnd5433@gmail.com>
2026-09-17 09:30:02 +0900

    feat: add theme repository

---
+++ src/main/java/com/ohgiraffers/quote_collection_spring/repository/category/ThemeRepository.java
@@ -0,0 +1,9 @@
+package com.ohgiraffers.quote_collection_spring.repository.category;
+
+import com.ohgiraffers.quote_collection_spring.entity.category.ThemeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ThemeRepository extends JpaRepository<ThemeEntity, Integer> {
+}
