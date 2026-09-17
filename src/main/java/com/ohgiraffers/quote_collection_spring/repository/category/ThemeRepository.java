ec456b93acfea5d7692a4635d2b3722439a86e69
parkchanwong <cksdnd5433@gmail.com>
2026-09-17 10:01:07 +0900

    feat: add return boolean theme exists name repository

--- src/main/java/com/ohgiraffers/quote_collection_spring/repository/category/ThemeRepository.java
+++ src/main/java/com/ohgiraffers/quote_collection_spring/repository/category/ThemeRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ThemeRepository extends JpaRepository<ThemeEntity, Integer> {
+    boolean existsByName(String name);
 }
