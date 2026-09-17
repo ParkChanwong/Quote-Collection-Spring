29f02217a4c43f107931dbac8caec3d5eb625cee
parkchanwong <cksdnd5433@gmail.com>
2026-09-17 11:18:28 +0900

    feat: add person find all service code

--- src/main/java/com/ohgiraffers/quote_collection_spring/service/person/PersonService.java
+++ src/main/java/com/ohgiraffers/quote_collection_spring/service/person/PersonService.java
@@ -1,8 +1,13 @@
 package com.ohgiraffers.quote_collection_spring.service.person;
 
+import com.ohgiraffers.quote_collection_spring.dto.person.PersonResponseDTO;
+import com.ohgiraffers.quote_collection_spring.entity.person.PersonEntity;
 import com.ohgiraffers.quote_collection_spring.repository.person.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PersonService {
@@ -13,5 +18,19 @@ public class PersonService {
         this.personRepository = personRepository;
     }
 
+    public PersonResponseDTO convertToDTO(PersonEntity personEntity) {
+        return new PersonResponseDTO(
+                personEntity.getId(),
+                personEntity.getCountry().getName(),
+                personEntity.getPeriod().getName(),
+                personEntity.getField().getName(),
+                personEntity.getName()
+        );
+    }
 
+    public List<PersonResponseDTO> findAllPersons() {
+        List<PersonEntity> persons = personRepository.findAll(Sort.by(Sort.Direction.ASC, "name"));
+
+        return persons.stream().map(this::convertToDTO).toList();
+    }
 }
