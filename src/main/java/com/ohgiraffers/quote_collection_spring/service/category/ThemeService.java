a2eb254cbcd000997a59b31de40d855b55fde64a
parkchanwong <cksdnd5433@gmail.com>
2026-09-17 09:47:39 +0900

    feat: add theme find by id Service code

--- src/main/java/com/ohgiraffers/quote_collection_spring/service/category/ThemeService.java
+++ src/main/java/com/ohgiraffers/quote_collection_spring/service/category/ThemeService.java
@@ -2,12 +2,14 @@ package com.ohgiraffers.quote_collection_spring.service.category;
 
 import com.ohgiraffers.quote_collection_spring.dto.category.ThemeDTO;
 import com.ohgiraffers.quote_collection_spring.entity.category.ThemeEntity;
+import com.ohgiraffers.quote_collection_spring.exception.category.theme.NotFoundThemeException;
 import com.ohgiraffers.quote_collection_spring.repository.category.ThemeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ThemeService {
@@ -26,5 +28,13 @@ public class ThemeService {
         List<ThemeEntity> themes = themeRepository.findAll(Sort.by(Sort.Direction.ASC, "name"));
 
         return themes.stream().map(this::convertToDTO).toList();
+    }
+
+    public ThemeDTO findThemeById(int id) {
+        ThemeEntity theme = themeRepository
+                .findById(id)
+                .orElseThrow(NotFoundThemeException::new);
+
+        return convertToDTO(theme);
     }
 }
