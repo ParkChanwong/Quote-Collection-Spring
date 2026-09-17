7714cd98d06a7a5bd6a6de892c8752e98c057f17
parkchanwong <cksdnd5433@gmail.com>
2026-09-17 17:09:18 +0900

    feat: add quote find by person name controller code

--- src/main/java/com/ohgiraffers/quote_collection_spring/controller/quote/QuoteController.java
+++ src/main/java/com/ohgiraffers/quote_collection_spring/controller/quote/QuoteController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,9 +23,18 @@ public class QuoteController {
         this.quoteService = quoteService;
     }
 
+    // 전체 명언 조회
     @GetMapping
     public ResponseEntity<ResponseList<QuoteResponseDTO>> findAllQuotes() {
         List<QuoteResponseDTO> quotes = quoteService.findAllQuotes();
+
+        return ResponseEntity.ok(new ResponseList<>(HttpStatus.OK.value(), quotes));
+    }
+
+    // 인물명으로 명언 조회
+    @GetMapping("/person")
+    public ResponseEntity<ResponseList<QuoteResponseDTO>> findQuoteByPerson(@RequestParam String keyword) {
+        List<QuoteResponseDTO> quotes = quoteService.findAllByPersonName(keyword.trim());
 
         return ResponseEntity.ok(new ResponseList<>(HttpStatus.OK.value(), quotes));
     }
