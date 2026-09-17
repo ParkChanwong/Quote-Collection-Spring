0ef29aada0ff2ea239e95b9de68a99728d1a6e41
parkchanwong <cksdnd5433@gmail.com>
2026-09-17 13:32:40 +0900

    feat: add person 국가명, 시대명, 분야명 조회 interface code

--- src/main/java/com/ohgiraffers/quote_collection_spring/repository/person/PersonRepository.java
+++ src/main/java/com/ohgiraffers/quote_collection_spring/repository/person/PersonRepository.java
@@ -4,6 +4,16 @@ import com.ohgiraffers.quote_collection_spring.entity.person.PersonEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PersonRepository extends JpaRepository<PersonEntity, Integer> {
+    // 국가명으로 인물 조회
+    List<PersonEntity> findByCountryNameContaining(String countryName);
+
+    // 시대명으로 인물 조회
+    List<PersonEntity> findByPeriodNameContaining(String periodName);
+
+    // 분야명으로 인물 조회
+    List<PersonEntity> findByFieldNameContaining(String fieldName);
 }
