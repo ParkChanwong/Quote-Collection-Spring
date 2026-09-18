05a02258fc46a808ab300c579cb9a4b5a823f930
parkchanwong <cksdnd5433@gmail.com>
2026-09-18 16:54:49 +0900

    feat: add bookmark cancel controller code

--- src/main/java/com/quotehunters/quotecollection/domain/mypage/bookmark/controller/BookmarkController.java
+++ src/main/java/com/quotehunters/quotecollection/domain/mypage/bookmark/controller/BookmarkController.java
@@ -4,14 +4,13 @@ import com.quotehunters.quotecollection.domain.mypage.bookmark.dto.BookmarkDTO;
 import com.quotehunters.quotecollection.domain.mypage.bookmark.entity.BookmarkEntity;
 import com.quotehunters.quotecollection.domain.mypage.bookmark.service.BookmarkService;
 import com.quotehunters.quotecollection.global.common.ResponseList;
+import com.quotehunters.quotecollection.global.common.ResponseSingle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Objects;
@@ -33,5 +32,15 @@ public class BookmarkController {
         List<BookmarkDTO> bookmarks = bookmarkService.findAllBookmarks(myId);
 
         return ResponseEntity.ok(new ResponseList<>(HttpStatus.OK.value(), bookmarks));
+    }
+
+    // 북마크 취소
+    @DeleteMapping("/bookmark/{bookmarkId}")
+    public ResponseEntity<ResponseSingle<String>> bookmarkCancel(@AuthenticationPrincipal Jwt jwt, @PathVariable int bookmarkId) {
+        int myId = Integer.parseInt(Objects.requireNonNull(jwt.getSubject()));
+
+        bookmarkService.bookmarkCancel(myId, bookmarkId);
+
+        return ResponseEntity.ok(new ResponseSingle<>(HttpStatus.OK.value(), "북마크가 취소되었습니다."));
     }
 }
