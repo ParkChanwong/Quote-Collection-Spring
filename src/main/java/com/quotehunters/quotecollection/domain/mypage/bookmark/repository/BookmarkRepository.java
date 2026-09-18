e7301334fc1e62c6905b5062e6a9b8dc5047efae
parkchanwong <cksdnd5433@gmail.com>
2026-09-18 16:30:23 +0900

    feat: add find my bookmark repository code

---
+++ src/main/java/com/quotehunters/quotecollection/domain/mypage/bookmark/repository/BookmarkRepository.java
@@ -0,0 +1,14 @@
+package com.quotehunters.quotecollection.domain.mypage.bookmark.repository;
+
+import com.quotehunters.quotecollection.domain.mypage.bookmark.entity.BookmarkEntity;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookmarkRepository extends JpaRepository<BookmarkEntity, Integer> {
+    // 내 모든 북마크 가져오기
+    List<BookmarkEntity> findAllByAccountId(int userId, Sort sort);
+}
