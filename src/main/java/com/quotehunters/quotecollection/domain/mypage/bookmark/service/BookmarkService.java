e1d97477db2ba1e8e1fb825ba5c675f8c88092ff
parkchanwong <cksdnd5433@gmail.com>
2026-09-18 16:54:39 +0900

    feat: add bookmark cancel service code

--- src/main/java/com/quotehunters/quotecollection/domain/mypage/bookmark/service/BookmarkService.java
+++ src/main/java/com/quotehunters/quotecollection/domain/mypage/bookmark/service/BookmarkService.java
@@ -2,6 +2,7 @@ package com.quotehunters.quotecollection.domain.mypage.bookmark.service;
 
 import com.quotehunters.quotecollection.domain.mypage.bookmark.dto.BookmarkDTO;
 import com.quotehunters.quotecollection.domain.mypage.bookmark.entity.BookmarkEntity;
+import com.quotehunters.quotecollection.domain.mypage.bookmark.exception.NotFoundBookmarkException;
 import com.quotehunters.quotecollection.domain.mypage.bookmark.repository.BookmarkRepository;
 import com.quotehunters.quotecollection.domain.person.entity.PersonEntity;
 import com.quotehunters.quotecollection.domain.person.exception.NotFoundPersonException;
@@ -24,7 +25,11 @@ public class BookmarkService {
     private final PersonRepository personRepository;
 
     @Autowired
-    private BookmarkService(BookmarkRepository bookmarkRepository, QuoteRepository quoteRepository, PersonRepository personRepository) {
+    private BookmarkService(
+            BookmarkRepository bookmarkRepository,
+            QuoteRepository quoteRepository,
+            PersonRepository personRepository
+    ) {
         this.bookmarkRepository = bookmarkRepository;
         this.quoteRepository = quoteRepository;
         this.personRepository = personRepository;
@@ -40,6 +45,7 @@ public class BookmarkService {
 
     private BookmarkDTO convertToDTO(BookmarkEntity bookmarkEntity) {
         return new BookmarkDTO(
+                bookmarkEntity.getId(),
                 bookmarkEntity.getQuote().getPerson().getName(),
                 bookmarkEntity.getQuote().getQuote()
         );
@@ -50,5 +56,16 @@ public class BookmarkService {
         List<BookmarkEntity> bookmarks = bookmarkRepository.findAllByAccountId(memberId, BOOKMARK_SORT);
 
         return bookmarks.stream().map(this::convertToDTO).toList();
+    }
+
+    // 북마크 취소
+    public void bookmarkCancel(int memberId, int bookmarkId) {
+        List<BookmarkEntity> bookmarks = bookmarkRepository.findAllByAccountId(memberId, BOOKMARK_SORT);
+
+        BookmarkEntity bookmark = bookmarks.stream().filter(b -> b.getId() == bookmarkId)
+                .findFirst()
+                .orElseThrow(NotFoundBookmarkException::new);
+
+        bookmarkRepository.delete(bookmark);
     }
 }
