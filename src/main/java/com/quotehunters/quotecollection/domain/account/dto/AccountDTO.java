aeb5ece091d5b11a92901da21cad7952f52f9f96
parkchanwong <cksdnd5433@gmail.com>
2026-09-18 17:31:48 +0900

    fix: toString에 비밀번호 삭제

--- src/main/java/com/quotehunters/quotecollection/domain/account/dto/AccountDTO.java
+++ src/main/java/com/quotehunters/quotecollection/domain/account/dto/AccountDTO.java
@@ -31,7 +31,6 @@ public class AccountDTO {
     public String toString() {
         return "SignInDTO{" +
                 "userId='" + userId + '\'' +
-                ", userPw='" + userPw + '\'' +
                 '}';
     }
 }
