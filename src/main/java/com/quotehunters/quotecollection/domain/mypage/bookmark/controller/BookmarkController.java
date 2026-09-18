dd85928cafec9a9f3bed4417bcfd45173144ec26
parkchanwong <cksdnd5433@gmail.com>
2026-09-18 16:30:46 +0900

    feat: add find my bookmark controller code

---
+++ src/main/java/com/quotehunters/quotecollection/domain/mypage/bookmark/controller/BookmarkController.java
@@ -0,0 +1,37 @@
+package com.quotehunters.quotecollection.domain.mypage.bookmark.controller;
+
+import com.quotehunters.quotecollection.domain.mypage.bookmark.dto.BookmarkDTO;
+import com.quotehunters.quotecollection.domain.mypage.bookmark.entity.BookmarkEntity;
+import com.quotehunters.quotecollection.domain.mypage.bookmark.service.BookmarkService;
+import com.quotehunters.quotecollection.global.common.ResponseList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/mypage")
+public class BookmarkController {
+    private final BookmarkService bookmarkService;
+
+    @Autowired
+    public BookmarkController(BookmarkService bookmarkService) {
+        this.bookmarkService = bookmarkService;
+    }
+
+    // 내 전체 북마크
+    @GetMapping("/bookmark")
+    public ResponseEntity<ResponseList<BookmarkDTO>> findAllBookmarks(@AuthenticationPrincipal Jwt jwt) {
+        int myId = Integer.parseInt(Objects.requireNonNull(jwt.getSubject()));
+        List<BookmarkDTO> bookmarks = bookmarkService.findAllBookmarks(myId);
+
+        return ResponseEntity.ok(new ResponseList<>(HttpStatus.OK.value(), bookmarks));
+    }
+}
